bf78ab77fabd1a4c91f4f70af42ef2b73e57044f
Komarov Alexander <sanrkom@gmail.com>
2020-05-03 20:40:32 +0200

    Update 2020-05-03T18:40:30.825Z

---
+++ precache-manifest.788727001b01815f3109c120793a7ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2b85fc870bd3fb943aeec04c8ee766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c85c659ab7ea4d0169",
+    "url": "/static/css/main.c261b060.chunk.css"
+  },
+  {
+    "revision": "22e317cafc14cfa259ec",
+    "url": "/static/js/2.038af1a4.chunk.js"
+  },
+  {
+    "revision": "29bd4e1c10813c0c14fca0c4aca4571b",
+    "url": "/static/js/2.038af1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c85c659ab7ea4d0169",
+    "url": "/static/js/main.d031d050.chunk.js"
+  },
+  {
+    "revision": "be22121c04ff9a5d980d",
+    "url": "/static/js/runtime-main.b1c1cfe2.js"
+  }
+]);
