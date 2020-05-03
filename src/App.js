3473262e1ba975f1d57ebaa3be60f653a8a5c2c0
Kossskom <ya.kom345@yandex.ru>
2020-05-03 21:30:18 +0300

    Add data

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const regions = data[0].regions;
 var maxCount = regions.reduce((acc, item) => item.id !== "ryazan" && item.sick > acc ? item.sick : acc,0);
 for (var i=0; i<regions.length; i++){
   const region = regions[i];
-  region.opacity = (region.sick / maxCount) - 0.4;
-  if(region.opacity !== 0 && region.opacity < 0.15){
-    region.opacity = 0.15;
+  region.opacity = (region.sick / maxCount);
+  if(region.sick !== 0 && region.opacity < 0.07){
+    region.opacity = 0.07;
   }
   if(region.id === "ryazan"){
     region.opacity = 1;
@@ -41,7 +41,7 @@ function App() {
   return (
     <div className="App">
       <SvgLoader path="./Ryazan_Oblast.svg">
-        {regions.map(r=> <SvgProxy selector={"#"+r.id} fill={`rgba(256, 0, 0, ${r.opacity})`} onElementSelected={(elem)=> hoverElem(elem, r)}></SvgProxy>)}
+        {regions.map(r=> <SvgProxy selector={"#"+r.id} fill={`rgba(${r.id === "ryazan" ? '140' : '180'}, 0, 0, ${r.opacity})`} onElementSelected={(elem)=> hoverElem(elem, r)}></SvgProxy>)}
       </SvgLoader>
   <div className="tooltip" ref={setPopperElement}>{popperContent}</div>
     </div>
