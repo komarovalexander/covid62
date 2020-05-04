1e8139356b45f85a1123d7d2ac8c16cdbffa8d52
Komarov Alexander <sanrkom@gmail.com>
2020-05-04 21:12:49 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { data } from './data';
 const regions = data[0].regions;
 
 var maxCount = regions.reduce((acc, item) => item.id !== "ryazan" && item.sick > acc ? item.sick : acc,0);
+var allSick = regions.reduce((acc, item) => item.sick + acc,0);
 for (var i=0; i<regions.length; i++){
   const region = regions[i];
   region.opacity = (region.sick / maxCount);
@@ -24,7 +25,11 @@ function App() {
   const [popperContent, setPopperContent] = React.useState(null);
   const hoverElem = (elem, region) => {
     elem.addEventListener("mouseover", () => {
-      setPopperContent(<div><div>{region.name}</div><div>Всего: {region.sick}</div> {region.description}</div>);
+      setPopperContent(<div>
+        <h4>{region.name}</h4>
+        <p>Всего: <b>{region.sick}</b></p>
+        <p>{region.description}</p>
+      </div>);
       createPopper(elem, popperElement, {
         placement: 'top',
         modifiers: [
@@ -40,10 +45,11 @@ function App() {
   }
   return (
     <div className="App">
+      <p>Всего заразившихся: {allSick}</p>
       <SvgLoader path="./Ryazan_Oblast.svg">
         {regions.map(r=> <SvgProxy selector={"#"+r.id} fill={`rgba(${r.id === "ryazan" ? '140' : '180'}, 0, 0, ${r.opacity})`} onElementSelected={(elem)=> hoverElem(elem, r)}></SvgProxy>)}
       </SvgLoader>
-  <div className="tooltip" ref={setPopperElement}>{popperContent}</div>
+      <div className="tooltip" ref={setPopperElement}>{popperContent}</div>
     </div>
   );
 }
