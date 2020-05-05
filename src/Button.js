5e1432e3c80dbdae1964fe7786376aaa757679ac
Kossskom <ya.kom345@yandex.ru>
2020-05-06 00:17:28 +0300

    Update

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+
+import React, { useState } from 'react';
+
+export default function Button({value}){
+    const [clicked, click] = useState(false);
+    return clicked ? <>{value}</> : value && <button onClick={() => click(true)}>Показать инфо по региону</button>;
+  }
--- src/CovidTable.js
+++ src/CovidTable.js
@@ -5,15 +5,17 @@ import React, { useState } from 'react';
 import { kaReducer, Table } from 'ka-table';
 import defaultOptions from 'ka-table/defaultOptions';
 import { DataType, SortingMode } from 'ka-table/enums';
+import Button from './Button';
 
 defaultOptions.columnSortDirection = 'descend';
+
 
 const tablePropsInit = {
   columns: [
     { key: 'name', title: 'Регион', dataType: DataType.String, style: {width: 150} },
     { key: 'sick', title: 'Зараженные', dataType: DataType.Number, style: {width: 100}, sortDirection: 'descend' },
     { key: 'lastDaySick', title: 'За сутки', dataType: DataType.Number, format: (value)=> value === 0 ? '' : `(+${value})`, style: {width: 100} },
-    { key: 'description', title: 'Доп. Инфо', dataType: DataType.String },
+    { key: 'description', title: 'Доп. Инфо', dataType: DataType.String, style: {width: 400}, cell: (props) => <Button {...props}/> },
   ],
   rowKeyField: 'id',
   sortingMode: SortingMode.Single,
