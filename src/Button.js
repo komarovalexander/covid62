b97643d72f73beabcc3ffaa055a7e1f5b43bf2bc
Kossskom <ya.kom345@yandex.ru>
2020-05-06 14:03:55 +0300

    day 3

--- src/Button.js
+++ src/Button.js
@@ -3,5 +3,5 @@ import React, { useState } from 'react';
 
 export default function Button({value}){
     const [clicked, click] = useState(false);
-    return clicked ? <>{value}</> : value && <button onClick={() => click(true)}>Показать инфо по региону</button>;
+    return clicked ? <>{value}</> : value && <button onClick={() => click(true)}>Показать инфо по району</button>;
   }
