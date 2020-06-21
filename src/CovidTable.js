25112dc30ffc301bcb2289c58ec3d582005cf55a
Komarov Alexander <sanrkom@gmail.com>
2020-06-21 21:51:18 +0200

    Update

--- src/CovidTable.js
+++ src/CovidTable.js
@@ -5,19 +5,29 @@ import React, { useState } from 'react';
 import { kaReducer, Table } from 'ka-table';
 import defaultOptions from 'ka-table/defaultOptions';
 import { DataType, SortingMode } from 'ka-table/enums';
-import Button from './Button';
+import DetailsButton from './Button';
 
 defaultOptions.columnSortDirection = 'descend';
 
+const DetailsRow = ({
+  rowData,
+}) => {
+  return (
+    <div>
+      {rowData.description}
+    </div>
+  );
+};
 
 const tablePropsInit = {
   columns: [
+    { key: 'description', style: {width: 120}, title: 'Доп. Инфо', dataType: DataType.String, cell: (props) => <DetailsButton {...props}/> },
     { key: 'name', title: 'Регион', dataType: DataType.String, style: {width: 150} },
     { key: 'sick', title: 'Зараженные', dataType: DataType.Number, style: {width: 90}, sortDirection: 'descend' },
     { key: 'lastDaySick', title: 'За сутки', dataType: DataType.Number, format: (value)=> value === 0 ? '' : `(+${value})`, style: {width: 80} },
-    { key: 'lastWeekSick', title: 'За неделю', dataType: DataType.Number, format: (value)=> !value ? '' : `(+${value})`, style: {width: 80} },
-    { key: 'description', title: 'Доп. Инфо', dataType: DataType.String, style: {width: 400}, cell: (props) => <Button {...props}/> },
+    { key: 'lastWeekSick', title: 'За неделю', dataType: DataType.Number, format: (value)=> !value ? '' : `(+${value})` },
   ],
+  detailsRow: DetailsRow,
   rowKeyField: 'id',
   sortingMode: SortingMode.Single,
 };
@@ -35,6 +45,13 @@ function CovidTable({ regions }) {
         data={regions}
         {...tableProps}
         dispatch={dispatch}
+        childAttributes={{
+          detailsRow: {
+            style: {
+              backgroundColor: '#eee'
+            }
+          }
+        }}
       />
     );
   };
