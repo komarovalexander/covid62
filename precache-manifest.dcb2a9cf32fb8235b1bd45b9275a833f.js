01a36c0b3951db00ee0eda7ff86e4f83f097d776
Kossskom <ya.kom345@yandex.ru>
2020-06-27 15:56:06 +0300

    Update 2020-06-27T12:56:02.948Z

---
+++ precache-manifest.dcb2a9cf32fb8235b1bd45b9275a833f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb4204c7307ef3d1ecae0d6fa8fc453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9dd3abbc6c8ff7239e",
+    "url": "/static/css/2.6ec2814a.chunk.css"
+  },
+  {
+    "revision": "5926d2a62ddff1ff58d0",
+    "url": "/static/css/main.68717870.chunk.css"
+  },
+  {
+    "revision": "af9dd3abbc6c8ff7239e",
+    "url": "/static/js/2.2646db03.chunk.js"
+  },
+  {
+    "revision": "29bd4e1c10813c0c14fca0c4aca4571b",
+    "url": "/static/js/2.2646db03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5926d2a62ddff1ff58d0",
+    "url": "/static/js/main.e73a6b82.chunk.js"
+  },
+  {
+    "revision": "be22121c04ff9a5d980d",
+    "url": "/static/js/runtime-main.b1c1cfe2.js"
+  }
+]);
